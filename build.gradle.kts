f123d03f816de71c6d1a4b57436d6d76e30848ef
fhemery <fhemery@hemit.fr>
2021-10-08 12:35:51 +0200

    Initial commit - generated from code.quarkus.io

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    kotlin("jvm") version "1.5.30"
+    kotlin("plugin.allopen") version "1.5.30"
+    id("io.quarkus")
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    /* implementation("io.quarkus:quarkus-hibernate-orm-panache-kotlin") */
+    implementation("io.quarkus:quarkus-kotlin")
+    implementation("io.quarkus:quarkus-jacoco")
+    implementation("io.quarkus:quarkus-resteasy")
+    /* implementation("io.quarkus:quarkus-jdbc-postgresql") */
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.quarkus:quarkus-arc")
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("io.rest-assured:rest-assured")
+}
+
+group = "org.hemit"
+version = "1.0.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+allOpen {
+    annotation("javax.ws.rs.Path")
+    annotation("javax.enterprise.context.ApplicationScoped")
+    annotation("io.quarkus.test.junit.QuarkusTest")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+    kotlinOptions.javaParameters = true
+}
