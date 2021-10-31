ab70ed1d78a976c08098d0d6b2ff583035c1e1a3
fhemery <fhemery@hemit.fr>
2021-10-31 22:53:31 +0100

    feat(tournament): fix architecture tests

--- src/test/kotlin/org/hemit/architecture/HexagonalArchitectureTest.kt
+++ src/test/kotlin/org/hemit/architecture/HexagonalArchitectureTest.kt
@@ -1,16 +1,19 @@
 package org.hemit.architecture
 
+import com.tngtech.archunit.core.importer.ImportOption
 import com.tngtech.archunit.junit.AnalyzeClasses
+import com.tngtech.archunit.junit.ArchTest
 import com.tngtech.archunit.lang.ArchRule
 import com.tngtech.archunit.library.Architectures.onionArchitecture
 
-@AnalyzeClasses(packages = ["org.hemit"])
+@AnalyzeClasses(packages = ["org.hemit"], importOptions = [ImportOption.DoNotIncludeTests::class])
 class HexagonalArchitectureTest {
-    //@ArchTest
+    @ArchTest
     val `Architecture should be a matching hexagonal architecture`: ArchRule =
         onionArchitecture()
-            .domainModels("org.hemit.domain.model..")
-            .domainServices("org.hemit.domain.service..")
-            .applicationServices("org.hemit.utils.ports.input..")
-
+            .withOptionalLayers(true)
+            .domainModels("org.hemit.domain..")
+            .adapter("api", "org.hemit.infra.api..")
+            .adapter("idgeneration", "org.hemit.infra.idgeneration..")
+            .adapter("storage", "org.hemit.infra.storage..")
 }
